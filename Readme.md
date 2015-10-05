1077a2cd5c506867055ea4425fdefc966e96718b
Paul Moore <p.f.moore@gmail.com>
2015-10-05 16:11:59 +0100

    Add command line parser

--- Readme.md
+++ Readme.md
@@ -4,3 +4,8 @@ Sim - A game simulator
 This program is a proof of concept implementation in Java of a Monte Carlo
 simulator of die rolls and similar game mechanics. It is based on the ideas
 behind a much more complete program, Monaco, written by Chris Dearlove.
+
+Example usage:
+```
+java -cp "externals/*;/Utils/Groovy/embeddable/groovy-all-2.4.4.jar;out/production/Sim" uk.org.enk.Sim --samples 122 -f expr.groovy
+```
--- src/uk/org/enk/Sim.java
+++ src/uk/org/enk/Sim.java
@@ -1,5 +1,6 @@
 package uk.org.enk;
 
+import org.apache.commons.cli.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,14 +19,25 @@ public class Sim {
         long start;
         long end;
         Outputter o;
+        CommandLine cmd = null;
+
+        Options options = new Options();
+        options.addOption("n", "samples", true, "Number of samples");
+        options.addOption("f", "file", true, "The expression script file");
+        CommandLineParser parser = new DefaultParser();
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("Encountered exception while parsing command line:\n" + e.getMessage());
+        }
 
         try {
-            script = new String(Files.readAllBytes(Paths.get(args[0])));
+            script = new String(Files.readAllBytes(Paths.get(cmd.getOptionValue("f"))));
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
-            N = Integer.parseInt(args[1]);
+            N = Integer.parseInt(cmd.getOptionValue("n", "10000"));
         } catch (NumberFormatException e) {
             System.out.format("Invalid number %s - using 1000000 instead%n", args[1]);
             N = 1000000;
@@ -33,7 +45,7 @@ public class Sim {
 
         sim = new GroovyTrial(script);
 
-        start = System.nanoTime();;
+        start = System.nanoTime();
         for (i = 0; i < N; ++i)
             results.add(sim.run());
         end = System.nanoTime();
